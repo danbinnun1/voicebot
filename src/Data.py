a7b1f5b1b503abcd6cbfe763f1dc30e5c2d36bd8
dan binnun <danbinnun1@gmail.com>
2020-09-30 13:21:39 +0300

    changed progress file to be sqlite instead of text

--- src/Data.py
+++ src/Data.py
@@ -1,22 +1,31 @@
 import os
 import zipfile
+import sqlite3
 
-_dataFolderPath='../data'
-recordingsFolderPath=os.path.join(_dataFolderPath,'recordings')
-temporalRecordingsFolderPath=os.path.join(_dataFolderPath,'temporalRecordings')
-progressFilePath=os.path.join(_dataFolderPath,'progress.txt')
+_dataFolderPath = '../data'
+recordingsFolderPath = os.path.join(_dataFolderPath, 'recordings')
+temporalRecordingsFolderPath = os.path.join(
+    _dataFolderPath, 'temporalRecordings')
+progressFilePath = str(os.path.join(_dataFolderPath, 'progress.db'))
+
 
 def _initializeData():
     if not os.path.isdir(_dataFolderPath):
         os.mkdir(_dataFolderPath)
         os.mkdir(recordingsFolderPath)
         os.mkdir(temporalRecordingsFolderPath)
-        progressFile=open(progressFilePath, 'w')
-        progressFile.close()
-    
+        conn = sqlite3.connect(progressFilePath)
+        c = conn.cursor()
+        c.execute('''
+        CREATE TABLE members (name text, progress text, password text);
+        ''')
+        conn.commit()
+        conn.close()
+
+
 def zipUserTone(username, tone, filepath):
     with zipfile.ZipFile(filepath, mode='w') as zipf:
-        dir_path=os.path.join(recordingsFolderPath,username,tone)
+        dir_path = os.path.join(recordingsFolderPath, username, tone)
         len_dir_path = len(dir_path)
         for root, _, files in os.walk(dir_path):
             for file in files:
