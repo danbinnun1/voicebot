351066ccedb3b1794d92273c61fd385cdc13f2a9
dan binnun <danbinnun1@gmail.com>
2020-09-30 22:05:21 +0300

    fixed bug in blconfig

--- src/BL/BLconfig.py
+++ src/BL/BLconfig.py
@@ -1,18 +1,10 @@
 _vowelsPath = '/home/danbinnun1/voicebot/settings/vowels.txt'
-
-vowels = []
-
-
 _tonesPath = '/home/danbinnun1/voicebot/settings/tones.txt'
-tones = []
 
-
-def _parseSettings():
-    with open(_vowelsPath, 'r') as vowelsFile:
-        vowels = vowelsFile.read().split(',')
-    vowelsFile.close()
-    with open(_tonesPath, 'r') as tonesFile:
-        tones = tonesFile.read().split(',')
-    tonesFile.close()
-
-_parseSettings()
+with open(_vowelsPath, 'r') as vowelsFile:
+    vowels = vowelsFile.read().split(',')
+vowelsFile.close()
+with open(_tonesPath, 'r') as tonesFile:
+    tones = tonesFile.read().split(',')
+tonesFile.close()
+print(tones)
