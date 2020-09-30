b1bbcea3dfcb9faae028a2ae3a8460642caa0afb
dan binnun <danbinnun1@gmail.com>
2020-10-01 02:14:24 +0300

    made test code cleaner

--- tests/DAL_test.py
+++ tests/DAL_test.py
@@ -2,15 +2,15 @@ import os
 import shutil
 import sys
 sys.path.append('..')
-import src.DAL.DALMember, src.DAL.Data
+from src.DAL import DALMember, Data
 
 datapath='./data'
 def test_insertMember():
     if os.path.isdir(datapath):
         shutil.rmtree(datapath)
-    src.DAL.Data.initializeData()
-    src.DAL.DALMember.insertMember('dan','123','a')
-    assert(src.DAL.DALMember.memberExists('dan', '123'))
-    assert(src.DAL.DALMember.getMemberProgress('dan')=='a')
-    src.DAL.DALMember.insertMember('1','2','3')
-    assert(src.DAL.DALMember.getAllMembers()==[('dan','a','123'),('1','3','2')])
+    Data.initializeData()
+    DALMember.insertMember('dan','123','a')
+    assert(DALMember.memberExists('dan', '123'))
+    assert(DALMember.getMemberProgress('dan')=='a')
+    DALMember.insertMember('1','2','3')
+    assert(DALMember.getAllMembers()==[('dan','a','123'),('1','3','2')])
