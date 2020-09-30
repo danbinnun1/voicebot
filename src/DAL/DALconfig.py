969c16ce7315a7dd62f971a6e288f12dcd36db7f
dan binnun <danbinnun1@gmail.com>
2020-09-30 18:50:08 +0300

    implemented initialize data function

--- src/DAL/DALconfig.py
+++ src/DAL/DALconfig.py
@@ -1,7 +1,7 @@
 import os
 
-_dataFolderPath = '/home/danbinnun1/voicebot/data'
-recordingsFolderPath = os.path.join(_dataFolderPath, 'recordings')
+dataFolderPath = '/home/danbinnun1/voicebot/data'
+recordingsFolderPath = os.path.join(dataFolderPath, 'recordings')
 temporalRecordingsFolderPath = os.path.join(
-    _dataFolderPath, 'temporalRecordings')
-progressFilePath = os.path.join(_dataFolderPath, 'progress.db')
+    dataFolderPath, 'temporalRecordings')
+progressFilePath = os.path.join(dataFolderPath, 'progress.db')
--- src/DAL/Data.py
+++ src/DAL/Data.py
@@ -1,36 +1,16 @@
+import DAL.DALconfig as config
 import os
-import zipfile
 import sqlite3
 
-_dataFolderPath = '/home/danbinnun1/voicebot/data'
-recordingsFolderPath = os.path.join(_dataFolderPath, 'recordings')
-temporalRecordingsFolderPath = os.path.join(
-    _dataFolderPath, 'temporalRecordings')
-progressFilePath = str(os.path.join(_dataFolderPath, 'progress.db'))
-
-
-def _initializeData():
-    if not os.path.isdir(_dataFolderPath):
-        os.mkdir(_dataFolderPath)
-        os.mkdir(recordingsFolderPath)
-        os.mkdir(temporalRecordingsFolderPath)
-        conn = sqlite3.connect(progressFilePath)
+def initializeData():
+    if not os.path.isdir(config.dataFolderPath):
+        os.mkdir(config.dataFolderPath)
+        os.mkdir(config.recordingsFolderPath)
+        os.mkdir(config.temporalRecordingsFolderPath)
+        conn = sqlite3.connect(config.progressFilePath)
         c = conn.cursor()
         c.execute('''
         CREATE TABLE members (name text PRIMARY KEY, progress text, password text);
         ''')
         conn.commit()
         conn.close()
-
-
-def zipUserTone(username, tone, filepath):
-    with zipfile.ZipFile(filepath, mode='w') as zipf:
-        dir_path = os.path.join(recordingsFolderPath, username, tone)
-        len_dir_path = len(dir_path)
-        for root, _, files in os.walk(dir_path):
-            for file in files:
-                file_path = os.path.join(root, file)
-                zipf.write(file_path, file_path[len_dir_path:])
-
-
-_initializeData()
