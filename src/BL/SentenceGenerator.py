23eca5a27bce80453bf987d052a0c10686b89961
dan binnun <danbinnun1@gmail.com>
2020-10-04 14:25:20 +0300

    added split word private method in sentence generator

--- src/BL/SentenceGenerator.py
+++ src/BL/SentenceGenerator.py
@@ -1,26 +1,46 @@
+import sys
+sys.path.append('.')
 from src.BL.SoundError import SoundError
 from src.BL.SoundException import SoundException
 from pydub import AudioSegment
 from src.BL.BLconfig import tones, vowels
 from src.DAL.DALRecording import getVowelRecording
 
+
 def generateSentence(name, sentence):
     space = ' '
-    if len(sentence) % 2 == 1:
-        raise SoundException(
-            SoundError.INVALID_SENTENCE)
-    i = 0
-    sentenceAudio = AudioSegment.silent(0)
-    while i < len(sentence):
-        syllable = sentence[i:i+2]
-        tone = syllable[0]
-        vowel = syllable[1]
-        if tone == space and vowel == space:
-            sentenceAudio = sentenceAudio+AudioSegment.silent(300)
-        elif not tone in tones or not vowel in vowels:
-            raise SoundException(
-                SoundError.INVALID_SENTENCE)
-        else:
-            sentenceAudio += getVowelRecording(name, tone, vowel)
-        i += 2
-    return sentenceAudio
+    words=sentence.split(space)
+    for word in words:
+        syllables=_splitBySyllables(word)
+
+    
+
+def _splitBySyllables(sentence):
+    syllables = []
+    if len(sentence) == 0:
+        return []
+    if len(sentence) == 1:
+        if sentence in tones:
+            return [(sentence, '.')]
+        if sentence in vowels:
+            return [('.', sentence)]
+        raise SoundException(SoundError.INVALID_SENTENCE)
+    if len(sentence) == 2:
+        if sentence in tones:
+            return [(sentence, '.')]
+        if sentence[0] in tones and sentence[1] in vowels:
+            return [(sentence[0], sentence[1])]
+        return _splitBySyllables(sentence[0]) + _splitBySyllables(sentence[1])
+    if sentence[0] in vowels:
+        return [('.', sentence[0])]+_splitBySyllables(sentence[1:])
+    if sentence[0:2] in tones:
+        if sentence[2] in vowels:
+            return [(sentence[0:2], sentence[2])]+_splitBySyllables(sentence[3:])
+        return [(sentence[0:2],'.')]+_splitBySyllables(sentence[2:])
+    if sentence[0] in tones:
+        if sentence[1] in vowels:
+            return [(sentence[0], sentence[1])]+_splitBySyllables(sentence[2:])
+        return [(sentence[0],'.')]+_splitBySyllables(sentence[1:])
+    raise SoundException(SoundError.INVALID_SENTENCE)
+
+print(_splitBySyllables(input()))
