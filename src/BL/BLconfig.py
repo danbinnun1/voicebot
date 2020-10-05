7988d7db62b2ec85fa2b0176534d9fc8a684bb49
dan binnun <danbinnun1@gmail.com>
2020-10-05 14:11:30 +0300

    changed bl config to be singleton

--- src/BL/BLconfig.py
+++ src/BL/BLconfig.py
@@ -1,10 +1,21 @@
 _vowelsPath = './settings/vowels.txt'
 _tonesPath = './settings/tones.txt'
 
-with open(_vowelsPath, 'r') as vowelsFile:
-    vowels = vowelsFile.read().split(',')
-vowelsFile.close()
-with open(_tonesPath, 'r') as tonesFile:
-    tones = tonesFile.read().split(',')
-tonesFile.close()
-print(tones)
+class BLconfig:
+    __vowels=None
+    __tones=None
+    @staticmethod
+    def getVowels():
+        if BLconfig.__vowels==None:
+            with open(_vowelsPath, 'r') as vowelsFile:
+                BLconfig.__vowels = vowelsFile.read().split(',')
+            vowelsFile.close()
+        return BLconfig.__vowels
+
+    @staticmethod
+    def getTones():
+        if BLconfig.__tones==None:
+            with open(_tonesPath, 'r') as tonesFile:
+                BLconfig.__tones = tonesFile.read().split(',')
+            tonesFile.close()
+        return BLconfig.__tones
